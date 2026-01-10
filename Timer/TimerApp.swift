c877bcd89d0adf25d46cc351003ee10ab36fc146
Grigory Don <dongrigory29@gmail.com>
2026-01-10 19:23:31 +0300

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by Grigory Don on 10.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
